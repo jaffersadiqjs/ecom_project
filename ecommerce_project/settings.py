97a896c2e7348fa120a3e04bb918700cc6947dc0
jaffersadiqjs <jaffersadiq2697@gmail.com>
2025-10-24 22:41:45 +0530

    Update settings.py

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ix)qvz4o+iqc8u-re*-my)(9@v)+iy$qa*cbn4*3_)b!7aqs2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
